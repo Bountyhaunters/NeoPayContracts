6ad5d666099a699a47a32b4a2a2a996b47f27211
vijay ankith <vijay2ankith@gmail.com>
2023-08-26 19:31:54 +0530

    added hardhat config

--- contracts/neo.sol
+++ contracts/neo.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NEO is ERC20 {
-    constructor() ERC20("NEO", "NEO") {
+    constructor() ERC20("NEO", "tNEO") {
         _mint(msg.sender, 10000000 * 10 ** decimals());
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  // defaultNetwork: "neo_evm_devnet",
+  networks: {
+    hardhat: {
+
+    },
+    neo_evm_devnet: {
+      url: "https://evm.ngd.network:32332",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
 };

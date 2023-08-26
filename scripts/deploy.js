c7e981a40d049126dbebf22b6215bebb39b9504b
vijay ankith <vijay2ankith@gmail.com>
2023-08-26 20:31:11 +0530

    updated deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,23 @@
-const hre = require("hardhat");
+const hre = require("hardhat"); 
+const {Web3} = require("web3");
 
 async function main() {
     const stack = await hre.ethers.deployContract("STACKOS");
     const neo = await hre.ethers.deployContract("NEO");
     
     await stack.waitForDeployment();
-    console.log("stack", stack.target);
+    console.log("stack", stack);
 
     await neo.waitForDeployment();
     console.log("neo", neo.target);
 
     const pool = await hre.ethers.deployContract("POOL", [stack.target, neo.target]);
     await pool.waitForDeployment();
-
     console.log("pool", pool.target);
+
+    await stack.approve(pool.target, '1000000000000000000000');
+    await neo.approve(pool.target, '1000000000000000000000');
+    await pool.addLiquidity('1000000000000000000000', '1000000000000000000000');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
